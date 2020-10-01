d5ab6aa2ae0c95fdae57637b1a71a09f32ad9a72
Rubino Andrea <andrea.rubino@drivek.com>
2020-10-01 16:47:23 +0200

    committo perchè coda vuole essere utile

---
+++ Intro.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var intro_text = [
+"""Hey novice.
+
+Your application request as \"quest giver\" of Cyberpunk 7720 has been accepted. 
+
+Congratulations.""",
+
+"""You should have already read the manual provided, but just to be sure I remind you how your work works.""",
+
+"""PROVA
+PROVA"""]
+
+var index = 0
+onready var label = $BackMonitorLayer/TextureRect/Text
+				
+func _ready() -> void:
+	label.text = intro_text[index]
+
+
+func _on_Enter_clicked() -> void:
+	index = index + 1
+	
+	if index >= intro_text.size():
+		get_tree().change_scene("res://src/scenes/Main.tscn")
+		return
+	
+	label.text = intro_text[index]
